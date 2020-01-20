3db5e3480e4db289f6f49f0822c646222f51a747
Sverre Stray Spetalen <sverress@stud.ntnu.no>
2020-01-20 15:28:47 +0100

    possible to set node value in gui

--- peg_solitaire/gui/components.py
+++ peg_solitaire/gui/components.py
@@ -2,13 +2,23 @@ from p5 import *
 
 
 class Node:
-    def __init__(self, x, y):
+    def __init__(self, x, y, value=1):
         self.x = x
         self.y = y
         self.size = 50
+        self.value = value  # 0 for empty 1 for filled
+
+    def set_value(self, value):
+        self.value = value
 
     def draw(self):
-        stroke(0)
-        fill(150)
+        stroke(100)
+        stroke_weight(3)
+        if self.value == 0:
+            no_fill()
+        else:
+            fill(150)
         ellipse_mode(CENTER)
         ellipse((self.x, self.y), self.size, self.size)
+
+
--- peg_solitaire/gui/main.py
+++ peg_solitaire/gui/main.py
@@ -2,25 +2,29 @@ from p5 import *
 from peg_solitaire.gui.components import *
 
 nodes = []
-SPACE = 100
-SIZE = 2
+SPACE = 80
+SIZE = 4
 
 
 def setup():
     size(600, 600)
     start = -(SIZE-1)*SPACE/2
     for i in range(0, SIZE):
+        row = []
         for j in range(0, SIZE):
-            nodes.append(Node(start + i * SPACE, start + j * SPACE))
+            row.append(Node(start + i * SPACE, start + j * SPACE))
+        nodes.append(row)
+    # Setting some node to EMPTY
+    nodes[2][2].value = 0
 
 
 def draw():
     background(255)
     translate(width/2, height/2)
     rotate(PI/4)
-    for node in nodes:
-        node.draw()
-
+    for row in nodes:
+        for node in row:
+            node.draw()
 
 
 if __name__ == '__main__':
