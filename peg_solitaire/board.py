277b14388e676d9c1b3c163afc6b73ab4ed31472
Sverre Stray Spetalen <sverress@stud.ntnu.no>
2020-01-20 17:32:46 +0100

    Board refractoring and clean up + legal actions

--- peg_solitaire/board.py
+++ peg_solitaire/board.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import math
+import numpy as np
 
 
 class Board(ABC):
@@ -12,17 +13,13 @@ class Board(ABC):
         :param board_size: Size of the board
         """
         self.board = []
-        self.initialize_board(board_size)
-
-    @abstractmethod
-    def initialize_board(self, board_size: int):
-        """
-        Implementation specific function to init board
-        :param board_size: Size of the board
-        """
-        pass
+        self.board_size = board_size
 
     def __str__(self):
+        """
+        Creates a simple toString for a board object
+        :return: string representation of the board
+        """
         output = ""
         for row in self.board:
             for node in row:
@@ -32,6 +29,7 @@ class Board(ABC):
 
     def get_cell(self, position: tuple):
         """
+        Method for getting a cell value. If the position is outside board None will be returned
         :param position: (x_index: int, y_index: int)
         :return: content of cell or None if the request is outside the board
         """
@@ -51,20 +49,34 @@ class Board(ABC):
         action = (str(w) for w in action)
         return ''.join(state) + ''.join(action)
 
-    def get_neighbors(self, position: tuple, board_size):
+    def filter_positions(self, position: tuple):
+        """
+        Standard filters for all position tuples in board class
+        :param position: (x_index: int, y_index: int)
+        :return: boolean describing if the position is within the board size
+        """
+        x, y = position
+        return 0 <= x < self.board_size and 0 <= y < self.board_size
+
+    def get_neighbors(self, position: tuple):
         """
         Gets all the neighbors for the given position of the board
         :param position: (x_index: int, y_index: int)
         :return: list of cell neighbors
         """
-        # Removing indices outside board
-        filtered_indices = filter(lambda pos: pos[0] >= 0 and pos[1] >= 0 and pos[0] < board_size and pos[1] < board_size, self.get_neighbors_indices(position, board_size))
         # Get cells and remove cells outside board (None cells)
-        cells = filter(lambda cell: cell is not None, map(lambda pos: self.get_cell(pos), filtered_indices))
-        return list(cells)
+        return list(
+            filter(
+                lambda cell: cell is not None,
+                map(
+                    lambda pos: self.get_cell(pos),
+                    self.get_neighbors_indices(position)
+                )
+            )
+        )
 
     def is_end_state(self):
-        actions = self.get_legal_actions(self, board_size)
+        actions = self.get_legal_actions()
         if not actions:
             return True
         return False
@@ -74,75 +86,74 @@ class Board(ABC):
         move_to = action[:-2]
         self.board[move_from] = 0
         self.board[move_to] = 1
-        self.board[]
-
+        raise NotImplemented
 
     @abstractmethod
     def get_neighbors_indices(self, position: tuple):
         """
+        Implementation specific method for different boards.
         :param position: (x_index: int, y_index: int)
         :return: list of positions: [(x_index_1: int, y_index_1: int), (x_index_2: int, y_index_2: int), ...]
         """
         pass
 
+    def get_legal_actions(self):
+        """
+        :return: a list of the legal actions from the current board state
+        """
+        actions = []
+        for parent_x, row in enumerate(self.board):
+            for parent_y, cell_value in enumerate(row):
+                # cell only relevant if empty
+                if cell_value == 0:
+                    indices_1st_gen_children = self.get_neighbors_indices((parent_x, parent_y))
+                    for indices_index, children_position in enumerate(indices_1st_gen_children):
+                        # 1st gen children must be filled
+                        if self.get_cell(children_position) == 1:
+                            parent_position = np.array((parent_x, parent_y))
+                            direction = np.array(children_position) - parent_position
+                            moving_cell_position = parent_position + 2*direction
+                            # 2st gen children must be filled
+                            if self.filter_positions(moving_cell_position) and self.get_cell(moving_cell_position) == 1:
+                                actions.append([moving_cell_position[0], moving_cell_position[1], parent_x, parent_y])
+        return actions
 
 
 class Diamond(Board):
-    def initialize_board(self, board_size):
+    def __init__(self, board_size):
+        super().__init__(board_size)
         for i in range(board_size):
             self.board.append([])
             for j in range(board_size):
                 self.board[i].append(1)
-        #temproary way of testing different states
+        # temporary way of testing different states
         self.board[math.floor((board_size-1)/2)][math.floor((board_size-1)/2)] = 0
         self.board[board_size - 1][board_size - 1] = 0
         self.board[board_size - 3][board_size - 1] = 0
         self.board[3][0] = 0
 
-    def get_neighbors_indices(self, position, board_size):
+    def get_neighbors_indices(self, position):
         r, c = position
         indices = [(r-1, c), (r-1, c+1), (r, c-1), (r, c+1), (r+1, c-1), (r+1, c)]
         # Removing indices outside the board
-        filtered_indices = filter(lambda pos: pos[0] >= 0 and pos[1] >= 0 and pos[0] < board_size and pos[1] < board_size, indices)
-        return list(filtered_indices)
-
-
-    def get_legal_actions(self, board_size):
-        actions = []
-        for row, valueR in enumerate(self.board):
-            for col, valueC in enumerate(valueR):
-                if valueC == 0:
-                    indicesN = self.get_neighbors_indices((row,col), board_size)
-                    for index, valueN in enumerate(self.get_neighbors((row, col), board_size)):
-                        if valueN == 1:
-                            indexN = indicesN[index]
-                            indexNN = (indexN[0] - row + indexN[0], indexN[1] - col + indexN[1])
-                            if indexNN[0] < 0 or indexNN[1] < 0 or indexNN[0] >= board_size or indexNN[1] >= board_size:
-                                continue
-                            if self.board[indexNN[0]][indexNN[1]] == 1:
-                                actions.append([indexNN[0], indexNN[1], row, col])
-        return actions
-
+        return list(filter(lambda pos: self.filter_positions(pos), indices))
 
 
 class Triangle(Board):
-    def initialize_board(self, board_size):
+    def __init__(self, board_size):
+        super().__init__(board_size)
         for i in range(board_size):
             self.board.append([])
             for j in range(i+1):
                 self.board[i].append(1)
 
-
-    def get_neighbors_indices(self, position, board_size):
+    def get_neighbors_indices(self, position):
         r, c = position
         indices = [(r-1, c-1), (r-1, c), (r, c-1), (r, c+1), (r+1, c), (r+1, c+1)]
         # Removing indices outside the board
-        filtered_indices = filter(lambda pos: pos[0] >= 0 and pos[1] >= 0 and pos[0] < board_size and pos[0] >= pos[1], indices)
-        return list(filtered_indices)
+        return list(filter(lambda pos: self.filter_positions(pos), indices))
 
-board_size = 4
-board = Diamond(board_size)
+
+board = Diamond(4)
 print(board)
-print(board.get_state())
-print(board.get_SAP([1,2,3,2]))
-print(board.get_legal_actions(board_size))
+print(board.get_legal_actions())
