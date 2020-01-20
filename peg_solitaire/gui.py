9f330721d991632e9d6f7e92e5a83344178f7f20
Sverre Stray Spetalen <sverress@stud.ntnu.no>
2020-01-20 09:15:21 +0100

    kivy exploration

--- peg_solitaire/gui.py
+++ peg_solitaire/gui.py
@@ -1,15 +1,28 @@
-import kivy
-kivy.require('1.0.6') # replace with your current kivy version !
-
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty, StringProperty
+from kivy.vector import Vector
+from kivy.graphics.vertex_instructions import (Rectangle,
+                                               Ellipse,
+                                               Line)
+from kivy.graphics.context_instructions import Color
 
 
-class MyApp(App):
+class PegSolitaire(Widget):
+    def __init__(self, **kwargs):
+        super(PegSolitaire, self).__init__(**kwargs)
+        with self.canvas:
+            Color(1, 0, 0, 1)
+            Ellipse(pos=(0, 100), size=(50, 50))
+            Color(0, 1, 0, 1)
+            Ellipse(pos=(100, 100), size=(50, 50))
+function grant access internett
+
+class PegSolitaireApp(App):
 
     def build(self):
-        return Label(text='Hello world')
+        return PegSolitaire()
 
 
 if __name__ == '__main__':
-    MyApp().run()
+    PegSolitaireApp().run()
