b5329285d1a2dfceca4fd601aa939f4f2bd82deb
Sverre Stray Spetalen <sverress@stud.ntnu.no>
2020-01-20 15:42:02 +0100

    Added comments

--- peg_solitaire/gui/main.py
+++ peg_solitaire/gui/main.py
@@ -1,17 +1,23 @@
 from p5 import *
 from peg_solitaire.gui.components import *
 
+# List of all nodes
 nodes = []
+# Distance between nodes
 SPACE = 80
+# Size of board
 SIZE = 4
 
 
 def setup():
+    # Setting size of screen
     size(600, 600)
+    # Start position of nodes to center properly
     start = -(SIZE-1)*SPACE/2
     for i in range(0, SIZE):
         row = []
         for j in range(0, SIZE):
+            # Creating nodes and adding space between them
             row.append(Node(start + i * SPACE, start + j * SPACE))
         nodes.append(row)
     # Setting some node to EMPTY
@@ -19,11 +25,15 @@ def setup():
 
 
 def draw():
+    # Setting background color
     background(255)
+    # Setting new reference point for drawing
     translate(width/2, height/2)
+    # Rotating around the new reference point
     rotate(PI/4)
     for row in nodes:
         for node in row:
+            # Drawing all the nodes
             node.draw()
 
 
