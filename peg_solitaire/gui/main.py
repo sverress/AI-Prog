0ecb5fe535649111416df5d15f28cce62595e6a2
Sverre Stray Spetalen <sverress@stud.ntnu.no>
2020-02-10 18:08:44 +0100

    parameter tuning

--- peg_solitaire/gui/main.py
+++ peg_solitaire/gui/main.py
@@ -67,7 +67,7 @@ def key_pressed():
 
 
 if __name__ == '__main__':
-    agent = Agent.create_agent_from_config_file("../parameters/triangle_5_21.json")
+    agent = Agent.create_agent_from_config_file("../parameters/triangle_5_32.json")
     agent.train(plot_result=True)
     # List of all nodes
     nodes = []
--- peg_solitaire/parameters/configs_working.md
+++ peg_solitaire/parameters/configs_working.md
@@ -3,7 +3,7 @@
 * [x] diamond_4_right_middle
 * [x] diamond_4_left_middle
 ### Triangle
-* [ ] triangle_5_21
-* [ ] triangle_5_32
-* [ ] triangle_5_31
+* [x] triangle_5_21
+* [x] triangle_5_32
+* [x] triangle_5_31
 
