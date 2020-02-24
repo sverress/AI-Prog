ef6cd337726e5e76a8140056e176b487fdf808b6
Jens Alfsen <jens.alfsen@gmail.com>
2020-02-24 18:43:23 +0100

    updated alle methods to use tuple state

--- mcts/StateManager.py
+++ mcts/StateManager.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 class StateManager(ABC):
     @staticmethod
     @abstractmethod
-    def generate_child_states(state: [int]):
+    def generate_child_states(state: ([int], bool)):
         """
         Takes in a parent state and returns the child states from this state
         :param state: list representing state of game
@@ -21,41 +21,49 @@ class StateManager(ABC):
 
     @staticmethod
     @abstractmethod
-    def is_end_state(state: [int]):
+    def is_end_state(state: ([int], bool)):
         """
         :param state: list representing state of game
         :return: a boolean stating if we are in end state
         """
 
+    @staticmethod
+    def state_to_string(state: ([int], bool)):
+        key = ""
+        for i in state[0]:
+            key += str(i)
+        key += str(state[1])
+        return key
 
 class Nim(StateManager):
     @staticmethod
-    def generate_child_states(state: [int]):
-        return [[i, state[1]] for i in range(state[0] - 1, state[0] - state[1], -1)]
+    def generate_child_states(state: ([int], bool)):
+        return [[i, state[0][1]] for i in range(state[0][0] - 1, state[0][0] - state[0][1], -1)]
 
     @staticmethod
     def init_game_state(**kwargs):
         return [kwargs.get('N'), kwargs.get('K')]
 
     @staticmethod
-    def is_end_state(state: [int]):
-        return state[0] == 0
+    def is_end_state(state: ([int], bool)):
+        return state[0][0] == 0
 
 
 class Lodge(StateManager):
     @staticmethod
-    def generate_child_states(state: [int]):
+    def generate_child_states(state: ([int], bool)):
         states = []
-        if state[0] > 0:
-            states.append([0]+state[1:])
-        for j in range(len(state)-1, 0, -1):
-            if state[j] == 0:
+        if state[0][0] > 0:
+            states.append(([0]+state[0][1:], not state[1]))
+        for j in range(len(state[0])-1, 0, -1):
+            if state[0][j] == 0:
                 continue
             i = j - 1
-            while state[i] == 0 and i >= 0:
+            while state[0][i] == 0 and i >= 0:
                 copy_state = state.copy()
-                copy_state[i] = copy_state[j]
-                copy_state[j] = 0
+                copy_state[0][i] = copy_state[0][j]
+                copy_state[0][j] = 0
+                copy_state[1] = not copy_state[1]
                 states.append(copy_state)
                 i -= 1
         return states
@@ -65,5 +73,5 @@ class Lodge(StateManager):
         return kwargs.get('B_init')
 
     @staticmethod
-    def is_end_state(state: [int]):
-        return state[0] == 2
+    def is_end_state(state: ([int], bool)):
+        return state[0][0] == 2
