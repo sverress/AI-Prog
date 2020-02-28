be30ef91bc1d68d47dd9da8819f8f27d7bce0129
Sverre Stray Spetalen <sverress@stud.ntnu.no>
2020-02-28 16:21:04 +0100

    renaming

--- mcts/run.py
+++ mcts/run.py
@@ -2,12 +2,17 @@ from mcts.GameSimulator import GameSimulator
 """
 FILE FOR SETTING UP A RUN OF THE MCTS ALGORITHM WITH CHOSEN PARAMETERS
 """
-g = 1  # number of games in a batch
-p = True  # starting-player option (True for we start, false for other start)
-m = 1  # number of simulations (and hence rollouts) per actual game move.
-n = 1  # starting number of pieces/stones in each game
-k = 1  # maximum number of pieces that either player can take on their turn.
-b_init = [0, 2, 0, 1]  # the initial board configuration.
+G = 1  # number of games in a batch
+P = True  # starting-player option (True for we start, false for other start)
+M = 1  # number of simulations (and hence rollouts) per actual game move.
 
-game = GameSimulator(g, p, m, n, k, b_init)
+# SETTINGS FOR NIM GAME
+PLAYING_NIM = False
+N = 1  # starting number of pieces/stones in each game
+K = 1  # maximum number of pieces that either player can take on their turn.
+
+# SETTINGS FOR
+B_INIT = [0, 2, 0, 1]  # the initial board configuration.
+
+game = GameSimulator(G, P, M, N, K, B_INIT)
 game.run()
