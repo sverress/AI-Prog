646799297a6191ef4ad7a7d33ea866ef3b153dc0
Sverre Stray Spetalen <sverress@stud.ntnu.no>
2020-02-28 16:42:59 +0100

    run setup for both games

--- mcts/GameSimulator.py
+++ mcts/GameSimulator.py
@@ -3,20 +3,24 @@ from mcts.MCTS import MCTS
 
 
 class GameSimulator:
-    def __init__(self, g, p, m, n, k, b_init: ([int], bool)):
+    NIM = "NIM"
+    LEDGE = "LEDGE"
+
+    def __init__(self, g, p, m, game, n, k, b_init: ([int], bool)):
         self.g = g
         self.p = p
         self.m = m
-        self.n = n
-        self.k = k
-        self.b_init = b_init
-        if not b_init:  # Is Nim
+        if game == GameSimulator.NIM:
             self.state_manager = Nim
-        else:  # Is ledge
+            self.init_state = self.state_manager.init_game_state(N=n, K=k, P=p)
+        if game == GameSimulator.LEDGE:
+            self.b_init = b_init
             self.state_manager = Lodge
+            self.init_state = self.state_manager.init_game_state(B_init=self.b_init, p=self.p)
 
     def run(self):
         for i in range(1, self.g + 1):
-            state = self.state_manager.init_game_state(B_init=self.b_init, p=self.p)
+            state = (self.init_state[0].copy(), self.init_state[1])  # copy state
             mcts = MCTS(state, self.state_manager)
             state = mcts.run()
+            print(f"iteration {i}: {state}")
--- mcts/run.py
+++ mcts/run.py
@@ -2,17 +2,19 @@ from mcts.GameSimulator import GameSimulator
 """
 FILE FOR SETTING UP A RUN OF THE MCTS ALGORITHM WITH CHOSEN PARAMETERS
 """
-G = 1  # number of games in a batch
+G = 10  # number of games in a batch
 P = True  # starting-player option (True for we start, false for other start)
-M = 1  # number of simulations (and hence rollouts) per actual game move.
+M = 100  # number of simulations (and hence rollouts) per actual game move.
+
+# Choosing witch game to play
+GAME = GameSimulator.LEDGE
 
 # SETTINGS FOR NIM GAME
-PLAYING_NIM = False
 N = 1  # starting number of pieces/stones in each game
 K = 1  # maximum number of pieces that either player can take on their turn.
 
-# SETTINGS FOR
+# SETTINGS FOR LEDGE
 B_INIT = [0, 2, 0, 1]  # the initial board configuration.
 
-game = GameSimulator(G, P, M, N, K, B_INIT)
+game = GameSimulator(G, P, M, GAME, N, K, B_INIT)
 game.run()
