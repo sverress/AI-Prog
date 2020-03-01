bacc5ab5a7f09c03be79a0abd4b5bdf512eb03ef
Sverre Stray Spetalen <sverress@stud.ntnu.no>
2020-03-01 17:20:01 +0100

    added verbose option in GameSimulator

--- mcts/run.py
+++ mcts/run.py
@@ -5,6 +5,7 @@ FILE FOR SETTING UP A RUN OF THE MCTS ALGORITHM WITH CHOSEN PARAMETERS
 G = 10  # number of games in a batch
 P = True  # starting-player option (True for we start, false for other start)
 M = 100  # number of simulations (and hence rollouts) per actual game move.
+verbose = False
 
 # Choosing witch game to play
 GAME = GameSimulator.NIM
@@ -16,5 +17,5 @@ K = 5  # maximum number of pieces that either player can take on their turn.
 # SETTINGS FOR LEDGE
 B_INIT = [0, 2, 0, 1, 0, 1, 0, 1, 0]  # the initial board configuration.
 
-game = GameSimulator(G, P, M, GAME, N, K, B_INIT)
+game = GameSimulator(G, P, M, GAME, N, K, B_INIT, verbose)
 game.run()
