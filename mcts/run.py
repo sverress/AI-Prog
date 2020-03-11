cb5bf3ff91ba041e99aef017459a51bc8cc05f5e
Sverre Stray Spetalen <sverress@stud.ntnu.no>
2020-03-11 12:10:04 +0100

    adding c config to run file

--- mcts/run.py
+++ mcts/run.py
@@ -7,6 +7,7 @@ P = StartingPlayerOptions.PLAYER_TWO  # starting-player option
 M = 500  # number of simulations (and hence rollouts) per actual game move.
 verbose = True
 max_tree_height = 10
+c = 1
 
 # Choosing witch game to play
 GAME = Games.LEDGE
@@ -18,5 +19,5 @@ K = 7  # maximum number of pieces that either player can take on their turn.
 # SETTINGS FOR LEDGE
 B_INIT = [0, 1, 0, 1, 0, 1, 2, 1, 0, 1]  # the initial board configuration.
 
-game = GameSimulator(G, P, M, GAME, N, K, B_INIT, verbose, max_tree_height)
+game = GameSimulator(G, P, M, GAME, N, K, B_INIT, verbose, max_tree_height, c=c)
 game.run()
