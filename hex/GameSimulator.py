ae75d86a3b5d0a564419d92d3d6712e2f89ca767
Sverre Stray Spetalen <sverress@stud.ntnu.no>
2020-03-23 12:56:51 +0100

    added MCTS mock

--- hex/GameSimulator.py
+++ hex/GameSimulator.py
@@ -22,6 +22,7 @@ class GameSimulator:
         self.state_manager = StateManager(self.k, self.p)
         self.current_state = self.state_manager.get_state()
         self.number_of_wins = 0
+        self.mcts = MockMCTS(self.current_state)
 
     def print_start_state(self, i):
         if self.verbose:
@@ -60,16 +61,27 @@ class GameSimulator:
         for i in range(1, self.g + 1):
             self.current_state = self.state_manager.get_state()
             self.print_start_state(i)
-            mcts = MCTS(
-                self.current_state, self.state_manager, self.max_tree_height, c=self.c
-            )
-            while not mcts.is_end_state(
+            while not self.mcts.is_end_state(
                 self.current_state
             ):  # Should be a call to the state manager
                 previous_state = self.current_state
-                self.current_state = mcts.run(self.m)  # , previous_state
-                mcts.root_state = self.current_state
-                mcts.cut_tree_at_state(self.current_state)
+                self.current_state = self.mcts.run(self.m)  # , previous_state
+                self.mcts.root_state = self.current_state
+                self.mcts.cut_tree_at_state(self.current_state)
                 self.print_move(previous_state)
                 self.update_winner_stats()
             self.print_winner_of_batch_game()
+
+
+class MockMCTS:
+    def __init__(self, state):
+        self.state = state
+        self.cut_tree_at_state = lambda s: None
+        self.n = 0
+
+    def run(self, m):
+        self.n += 1
+        return self.state
+
+    def is_end_state(self, s):
+        return self.n > 3
