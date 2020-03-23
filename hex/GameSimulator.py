186638ce0e428548d17430d99e29b11c7397d86d
Sverre Stray Spetalen <sverress@stud.ntnu.no>
2020-03-23 16:56:08 +0100

    anet setup

--- hex/GameSimulator.py
+++ hex/GameSimulator.py
@@ -1,7 +1,9 @@
 from hex.StateManager import StateManager
+from hex.ANET import ANET
 from hex.MCTS import MCTS
 from libs.helpers import print_loader
 import enum
+import random
 
 
 class StartingPlayerOptions(enum.Enum):
@@ -23,6 +25,7 @@ class GameSimulator:
         self.current_state = self.state_manager.get_state()
         self.number_of_wins = 0
         self.mcts = MockMCTS(self.current_state)
+        self.anet = ANET(k)
 
     def print_start_state(self, i):
         if self.verbose:
@@ -47,10 +50,10 @@ class GameSimulator:
                 f"Player {2 if self.state_manager.is_P1(self.current_state) else 1} wins the game"
             )
 
-    def print_run_summary(self, number_of_wins):
+    def print_run_summary(self):
         print("\n------------- SUMMARY -------------")
         print(
-            f"Player 1 wins {number_of_wins} games out of {self.g}. ({round((number_of_wins / self.g) * 100)}%)"
+            f"Player 1 wins {self.number_of_wins} games out of {self.g}. ({round((self.number_of_wins / self.g) * 100)}%)"
         )
 
     def update_winner_stats(self):
@@ -64,13 +67,24 @@ class GameSimulator:
             while not self.mcts.is_end_state(
                 self.current_state
             ):  # Should be a call to the state manager
+
+                # Get distribution and add the case to anet
+                distribution_of_visit_counts = self.mcts.run(self.m)
+                self.anet.add_case(self.current_state, distribution_of_visit_counts)
+
+                # Choose action and reset tree (Reset mcts tree lines should be done in MCTS class?)
                 previous_state = self.current_state
-                self.current_state = self.mcts.run(self.m)  # , previous_state
+                move = argmax(distribution_of_visit_counts)
+                self.current_state = do_move(self.current_state, move)
                 self.mcts.root_state = self.current_state
                 self.mcts.cut_tree_at_state(self.current_state)
+
                 self.print_move(previous_state)
-                self.update_winner_stats()
+
+            self.update_winner_stats()
+            self.anet.train()
             self.print_winner_of_batch_game()
+        self.print_run_summary()
 
 
 class MockMCTS:
@@ -80,8 +94,37 @@ class MockMCTS:
         self.n = 0
 
     def run(self, m):
+        """
+        returns random distribution of visit counts
+        :param m:
+        :return:
+        """
         self.n += 1
-        return self.state
+        counts = [random.randint(0, m) for i in range(0, len(self.state[:-2]))]
+        return [count / sum(counts) for count in counts]
 
     def is_end_state(self, s):
-        return self.n > 3
+        done = self.n > random.randint(15, 30)
+        if done:
+            self.n = 0
+        return done
+
+
+def argmax(liste):
+    best = -float("Inf")
+    best_i = -1
+    for i in range(0, len(liste)):
+        if liste[i] > best:
+            best = liste[i]
+            best_i = i
+    return best_i
+
+
+def do_move(state: str, move_index: int):
+    output = ""
+    for i, char in enumerate(state[:-2]):
+        if i == move_index:
+            output += state[-1]
+        else:
+            output += char
+    return f"{output}:{1 if int(state[-1])==2 else 2}"
--- hex/run.py
+++ hex/run.py
@@ -5,7 +5,7 @@ FILE FOR SETTING UP A RUN OF THE MCTS ALGORITHM WITH PARAMETERS
 """
 G = 15  # number of games in a batch
 P = StartingPlayerOptions.P1  # starting-player option
-M = 2  # number of simulations (and hence rollouts) per actual game move.
+M = 20  # number of simulations (and hence rollouts) per actual game move.
 verbose = False
 max_tree_height = 25
 c = 1
