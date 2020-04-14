a63574920261acc85ceae7993aa9c666f2ae4e1d
Sverre Stray Spetalen <sverress@stud.ntnu.no>
2020-04-14 16:36:47 +0200

    state tree 1.0

--- hex/MCTS.py
+++ hex/MCTS.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import random
 import math
 import numpy as np
+import enum
 
 from hex.StateManager import StateManager
 
@@ -10,9 +11,11 @@ from hex.StateManager import StateManager
 class MCTS:
     def __init__(self, state_manager: StateManager, actor_net, max_tree_height=5, c=1):
         self.state_manager = state_manager
-        self.G = nx.DiGraph()
+        self.tree = StateTree()
         self.root_state = self.state_manager.get_state()
-        self.add_node(self.root_state, end_state=self.state_manager.is_end_state())
+        self.tree.add_state_node(
+            self.root_state, is_end_state=self.state_manager.is_end_state()
+        )
         self.c = c
         self.max_tree_height = max_tree_height
         self.actor_net = actor_net
@@ -27,15 +30,16 @@ class MCTS:
 
         split_board = lambda input_state: input_state.split(":")
         parent_board, parent_player = split_board(state)
-        child_states = self.get_child_states(state)
+        child_states = self.tree.get_child_states(state)
         change_indices_dict = {}
         total_visits = 0
         for child in child_states:
             child_board, child_player = split_board(child)
             for i in range(len(child_board)):
                 if parent_board[i] != child_board[i]:
-                    change_indices_dict[i] = self.G.nodes[child]["n"]
-                    total_visits += self.G.nodes[child]["n"]
+                    child_number_of_visits = self.tree.get_state_number_of_visits(child)
+                    change_indices_dict[i] = child_number_of_visits
+                    total_visits += child_number_of_visits
                     break
 
         return [
@@ -59,26 +63,28 @@ class MCTS:
         self.actor_net.add_case(self.root_state, distribution.copy())
         child = self.greedy_best_child(self.root_state)
         self.root_state = child
-        self.cut_tree_at_state(child)
+        self.tree.cut_at_state(child)
         return child
 
     def traverse_tree(self, state: str, depth):
-        if depth == self.max_tree_height or self.is_end_state(state):
+        if depth == self.max_tree_height or self.tree.is_end_state(state):
             self.simulate(state)
             return
-        out_edgs = list(self.G.out_edges(state, data=True))
-        if not out_edgs:
+        outgoing_edges = self.tree.get_outgoing_edges(state)
+        if not outgoing_edges:
             self.expand(state)
             return
-        unvisited_out_edgs = list(filter(lambda x: x[2]["n"] == 0, out_edgs))
-        if unvisited_out_edgs:
-            child = random.choice(unvisited_out_edgs)[1]
+        unvisited_outgoing_edges = list(
+            filter(lambda x: x[2]["n"] == 0, outgoing_edges)
+        )
+        if unvisited_outgoing_edges:
+            child = random.choice(unvisited_outgoing_edges)[1]
             self.state_manager.set_state_manager(child)
-            self.G.nodes[child]["end_state"] = self.state_manager.is_end_state()
-            self.G.get_edge_data(state, child)["flag"] = 1
+            self.tree.set_end_state(child, self.state_manager.is_end_state())
+            self.tree.set_active_edge(state, child, True)
             self.simulate(child)
             return
-        child = self.best_child_uct(state, out_edgs)
+        child = self.best_child_uct(state, outgoing_edges)
         self.state_manager.set_state_manager(child)
         self.traverse_tree(child, depth + 1)
 
@@ -86,25 +92,21 @@ class MCTS:
         children = StateManager.generate_child_states(state)
         for child in children:
             # Could this cause some problems?
-            if child in self.G.nodes:
-                self.add_edge(state, child)
+            if child in self.tree.get_nodes():
+                self.tree.add_edge(state, child)
             else:
-                self.add_node(child)
-                self.add_edge(state, child)
+                self.tree.add_state_node(child)
+                self.tree.add_edge(state, child)
         chosen_child = random.choice(children)
         self.state_manager.set_state_manager(chosen_child)
-        self.G.nodes[chosen_child]["end_state"] = self.state_manager.is_end_state()
-        self.G.get_edge_data(state, chosen_child)["flag"] = 1
+        self.tree.set_end_state(chosen_child, self.state_manager.is_end_state())
+        self.tree.set_active_edge(state, chosen_child, True)
         self.simulate(chosen_child)
 
     def greedy_best_child(self, state: str) -> str:
-        sorted_list = sorted(
-            self.G.out_edges(state, data=True),
-            key=lambda x: x[2]["sap_value"],
-            reverse=True,
-        )
+        sorted_list = self.tree.get_outgoing_edges(state, sorted_by_saps=True)
         if self.state_manager.get_player(state) == 1:
-            best_child = sorted_list[0][1]
+            best_child = sorted_list[0][1]  # Why is this the best one?
             self.state_manager.set_state_manager(best_child)
             return best_child
         else:
@@ -113,7 +115,7 @@ class MCTS:
             return best_child
 
     def best_child_uct(self, state: str, out_edgs) -> str:
-        node_n = self.G.nodes[state]["n"]
+        node_n = self.tree.get_state_number_of_visits(state)
         if self.state_manager.get_player(state) == 1:
             best_edge = max(
                 out_edgs,
@@ -139,17 +141,17 @@ class MCTS:
         """
         start_state = state
         while not self.state_manager.is_end_state():
-            out_edgs = list(self.G.out_edges(state))
-            if not out_edgs:
+            outgoing_edges = list(self.tree.get_outgoing_edges(state))
+            if not outgoing_edges:
                 children = StateManager.generate_child_states(state)
                 for child in children:
-                    if child in self.G.nodes:
-                        self.add_edge(state, child)
+                    if child in self.tree.get_nodes():
+                        self.tree.add_edge(state, child)
                     else:
-                        self.add_node(child)
-                        self.add_edge(state, child)
-                out_edgs = list(self.G.out_edges(state))
-            state = random.choice(out_edgs)[1]
+                        self.tree.add_state_node(child)
+                        self.tree.add_edge(state, child)
+                outgoing_edges = self.tree.get_outgoing_edges(state)
+            state = random.choice(outgoing_edges)[1]
         win_player1 = -1 if self.state_manager.get_player(state) == 1 else 1
         self.backpropagate(start_state, win_player1)
 
@@ -161,7 +163,9 @@ class MCTS:
         else:
             # Choose random state from those with positive probability
             # prob == 0 might be occupied cells on the board
-            chosen_index = random.choice([i[0] for i, prob in np.ndenumerate(distribution) if prob > 0])
+            chosen_index = random.choice(
+                [i[0] for i, prob in np.ndenumerate(distribution) if prob > 0]
+            )
         return StateManager.get_next_state_from_distribution_position(
             chosen_index, state
         )
@@ -188,29 +192,100 @@ class MCTS:
 
     def backpropagate(self, state: str, win_player1: int):
         if state == self.root_state:
-            self.G.nodes[state]["n"] += 1
+            self.tree.increment_state_number_of_visits(state)
             return
-        parent_state = self.get_parent(state)
+        parent_state = self.tree.get_parent(state)
 
-        self.G.nodes[state]["n"] += 1
-        self.G.get_edge_data(parent_state, state)["n"] += 1
-        edge_times_enc = self.G.get_edge_data(parent_state, state)["n"]
-        edge_sap_value = self.G.get_edge_data(parent_state, state)["sap_value"]
-        self.G.get_edge_data(parent_state, state)["sap_value"] += (
-            win_player1 - edge_sap_value
-        ) / edge_times_enc
-        self.G.get_edge_data(parent_state, state)["flag"] = 0
+        self.tree.increment_state_number_of_visits(state)
+        self.tree.increment_edge_number_of_visits(parent_state, state)
+        edge_times_enc = self.tree.get_edge_number_of_visits(parent_state, state)
+        edge_sap_value = self.tree.get_sap_value(parent_state, state)
+        new_sap_value = (
+            self.tree.get_sap_value(parent_state, state)
+            + (win_player1 - edge_sap_value) / edge_times_enc
+        )
+        self.tree.set_sap_value(parent_state, state, new_sap_value)
+        self.tree.set_active_edge(parent_state, state, False)
 
         self.backpropagate(parent_state, win_player1)
 
-    # GRAPH METHODS
 
-    def add_node(self, state: str, end_state=None):
+class TreeConstants(enum.Enum):
+    # Node attributes
+    IS_END_STATE = "is_end_state"
+
+    # Edge attributes
+    SAP_VALUE = "sap_value"
+    IS_ACTIVE = "is_active"
+
+    # Both
+    NUMBER_OF_VISITS = "n"
+
+    # Graph Colors
+    PLAYER1_COLOR = "b"
+    PLAYER2_COLOR = "r"
+
+
+class StateTree:
+    def __init__(self):
+        self.graph = nx.DiGraph()
+
+    def get_nodes(self):
+        return self.graph.nodes
+
+    def add_state_node(self, state: str, is_end_state=False):
         """
         Adds node to the DiGraph G with initial number of encounters to zero
-        :param state: (list representing board state, player to move): ([int], bool)
+        :param state: string representation of state
+        :param is_end_state: boolean stating if the new state is an end state
         """
-        self.G.add_node(state, n=0, end_state=end_state)
+
+        self.graph.add_node(
+            state,
+            **{
+                TreeConstants.NUMBER_OF_VISITS: 0,
+                TreeConstants.IS_END_STATE: is_end_state,
+            }
+        )
+
+    def get_child_states(self, state: str) -> [str]:
+        return list(self.graph.successors(state))
+
+    def get_state_number_of_visits(self, state: str) -> int:
+        return self.graph.nodes[state][TreeConstants.NUMBER_OF_VISITS]
+
+    def increment_state_number_of_visits(self, state: str) -> None:
+        self.graph.nodes[state][TreeConstants.NUMBER_OF_VISITS] += 1
+
+    def cut_at_state(self, state: str) -> None:
+        sub_tree_nodes = nx.bfs_tree(self.graph, state)
+        self.graph = nx.DiGraph(self.graph.subgraph(sub_tree_nodes))
+
+    @staticmethod
+    def edge_is_unvisited(edge):
+        return edge[2][TreeConstants.NUMBER_OF_VISITS] == 0
+
+    def get_outgoing_edges(self, state: str, unvisited=False, sorted_by_saps=False):
+        # TODO: Get output format of this function
+
+        outgoing_edges = list(self.graph.out_edges(state))
+        if sorted_by_saps:
+            outgoing_edges = sorted(
+                self.graph.out_edges(state, data=True),
+                key=lambda x: x[2]["sap_value"],
+                reverse=True,
+            )
+        if unvisited:
+            outgoing_edges = [
+                edge for edge in outgoing_edges if StateTree.edge_is_unvisited(edge)
+            ]
+        return outgoing_edges
+
+    def is_end_state(self, state: str) -> bool:
+        return self.graph.nodes[state][TreeConstants.IS_END_STATE]
+
+    def set_end_state(self, state: str, value: bool) -> None:
+        self.graph.nodes[state][TreeConstants.IS_END_STATE] = value
 
     def add_edge(self, parent_state, child_state):
         """
@@ -219,55 +294,99 @@ class MCTS:
         :param parent_state: (list representing board state, player to move): ([int], bool)
         :param child_state: (list representing board state, player to move): ([int], bool)
         """
-        self.G.add_edge(parent_state, child_state, sap_value=0, n=0, flag=0)
+        self.graph.add_edge(
+            parent_state,
+            child_state,
+            **{
+                TreeConstants.SAP_VALUE: 0.0,
+                TreeConstants.NUMBER_OF_VISITS: 0,
+                TreeConstants.IS_ACTIVE: 0,
+            }
+        )
 
-    def get_child_states(self, state):
-        return list(self.G.successors(state))
+    def get_sap_value(self, parent_state: str, child_state: str) -> float:
+        return self.graph.get_edge_data(parent_state, child_state)[
+            TreeConstants.SAP_VALUE
+        ]
+
+    def set_sap_value(self, parent_state: str, child_state: str, value: float) -> None:
+        self.graph.get_edge_data(parent_state, child_state)[
+            TreeConstants.SAP_VALUE
+        ] = value
+
+    def set_active_edge(self, parent_state: str, child_state: str, value: bool) -> None:
+        """
+        This is what previously was called a `flag` meaning this
+            edge was used in the latest tree traversal
+        """
+        self.graph.get_edge_data(parent_state, child_state)[
+            TreeConstants.IS_ACTIVE
+        ] = value
+
+    def is_active_edge(self, parent_state: str, child_state: str) -> bool:
+        """
+        This is what previously was called a `flag` meaning this
+            edge was used in the latest tree traversal
+        """
+        return self.graph.get_edge_data(parent_state, child_state)[
+            TreeConstants.IS_ACTIVE
+        ]
+
+    def get_edge_number_of_visits(self, parent_state: str, child_state: str) -> int:
+        return self.graph.get_edge_data(parent_state, child_state)[
+            TreeConstants.NUMBER_OF_VISITS
+        ]
+
+    def increment_edge_number_of_visits(
+        self, parent_state: str, child_state: str
+    ) -> None:
+        self.graph.get_edge_data(parent_state, child_state)[
+            TreeConstants.NUMBER_OF_VISITS
+        ] += 1
 
     def get_parent(self, state: str) -> str:
-        parent_list = list(self.G.predecessors(state))
+        parent_list = list(self.graph.predecessors(state))
         if len(parent_list) == 1:
             return parent_list[0]
         else:
-            flagged_parent_list = [
-                parent
-                for parent in parent_list
-                if self.G.get_edge_data(parent, state)["flag"] == 1
+            active_parent_list = [
+                parent for parent in parent_list if self.is_active_edge(parent, state)
             ]
-            if len(flagged_parent_list) == 1:
-                return flagged_parent_list[0]
+            if len(active_parent_list) == 1:
+                return active_parent_list[0]
             else:
                 raise ValueError(
                     "More than one parent of input state with positive flag"
                 )
 
-    def cut_tree_at_state(self, state: str):
-        sub_tree_nodes = nx.bfs_tree(self.G, state)
-        self.G = nx.DiGraph(self.G.subgraph(sub_tree_nodes))
-
-    def is_end_state(self, state):
-        return self.G.nodes[state]["end_state"]
-
-    def print_graph(self):
+    def print_graph(self, state_manager):
         """
         Print the DiGraph object representing the current tree
         """
-        pos = nx.shell_layout(self.G)
+        pos = nx.shell_layout(self.graph)
         blue_player_nodes = []
         red_player_nodes = []
         labels = {}
-        for state in self.G.nodes:
-            labels[state] = self.state_manager.graph_label(state)
-            if self.state_manager.get_player(state) == 1:
+        for state in self.graph.nodes:
+            labels[state] = state_manager.graph_label(state)
+            if state_manager.get_player(state) == 1:
                 blue_player_nodes.append(state)
             else:
                 red_player_nodes.append(state)
         nx.draw_networkx_nodes(
-            self.G, pos, nodelist=blue_player_nodes, node_color="b", alpha=0.5
+            self.graph,
+            pos,
+            nodelist=blue_player_nodes,
+            node_color=TreeConstants.PLAYER1_COLOR,
+            alpha=0.5,
         )
         nx.draw_networkx_nodes(
-            self.G, pos, nodelist=red_player_nodes, node_color="r", alpha=0.5
+            self.graph,
+            pos,
+            nodelist=red_player_nodes,
+            node_color=TreeConstants.PLAYER2_COLOR,
+            alpha=0.5,
         )
-        nx.draw_networkx_edges(self.G, pos)
-        nx.draw_networkx_labels(self.G, pos, labels, font_size=10)
+        nx.draw_networkx_edges(self.graph, pos)
+        nx.draw_networkx_labels(self.graph, pos, labels, font_size=10)
         plt.show()
