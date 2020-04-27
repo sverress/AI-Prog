e6207445125535d62effe77ac3a2e952c45ca598
Sverre Stray Spetalen <sverress@stud.ntnu.no>
2020-04-27 17:05:26 +0200

    loader computes minimum remaining time

--- hex/run.py
+++ hex/run.py
@@ -8,26 +8,26 @@ FILE FOR SETTING UP A RUN OF THE MCTS ALGORITHM WITH PARAMETERS
 """
 G = 100  # number of games in a batch
 P = StartingPlayerOptions.P1  # starting-player option
-verbose = True
+verbose = False
 save_interval = 50  # number of games between each time we save a model
 
 # SETTINGS FOR HEX
-k = 3  # board size kxk, 3 <= k <= 10
+k = 4  # board size kxk, 3 <= k <= 10
 
 actor_net_parameters = {
     "buffer_batch_size": 350,
     "max_size_buffer": 3000,
     "replay_buffer_cutoff_rate": 0.3,
-    "epochs": 10,
+    "epochs": 100,
     "verbose": 2 if verbose else 0,  # 2: one line per epoch
     "save_directory": "trained_models",
-    "hidden_layers_structure": [(k ** 2 + 10) * 8, (k ** 2 + 10) * 8, (k ** 2 + 10) * 8],
+    "hidden_layers_structure": [(k ** 2 + 10) * 2, int((k ** 2 + 10) * 0.8), int((k ** 2 + 10) * 0.6)],
     "learning_rate": 0.001,
 }
 mcts_parameters = {
     "max_tree_height": 8,
     "c": 1.5,  # Exploration constant
-    "number_of_simulations": 10,  # number of simulations (and hence roll-outs) per actual game move
+    "number_of_simulations": 20,  # number of simulations (and hence roll-outs) per actual game move
     "verbose": verbose,
 }
 
